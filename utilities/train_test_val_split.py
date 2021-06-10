90b50b26f60eddca6c1a00bef5f0af944bfda09c
Siddhi <brahmbhattsiddhi@gmail.com>
2021-06-10 14:28:31 +0000

    added: useful utilities

---
+++ utilities/train_test_val_split.py
@@ -0,0 +1,42 @@
+# %%
+#importing necessary libraries
+import numpy as np
+from random import randint
+import random
+
+
+# %%
+user = 'aws'
+
+if user == 'siddhi':
+    path_videos = 'C:/RoadCrossingAssistant/Data/Videos/'
+    path_labels_csv = 'C:/RoadCrossingAssistant/Data/labels_framewise_csv.csv'
+    path_labels_list = 'C:/RoadCrossingAssistant/Data/labels_framewise_list.pkl'
+    path_frames = 'C:/RoadCrossingAssistant/Data/Frames/'
+
+elif user == 'yagnesh':
+    path_videos = '/home/yagnesh/Study/Machine Learning/ML projects/RoadCrossingAssistant_Arrays/videos/'
+    path_labels_csv = '/home/yagnesh/Study/Machine Learning/ML projects/RoadCrossingAssistant_Arrays/labels_framewise.csv'
+    path_labels_list = '/home/yagnesh/Study/Machine Learning/ML projects/RoadCrossingAssistant_Arrays/labels_framewise.pkl'
+
+elif user == 'aws':
+    path_videos = '/home/ubuntu/Data/Videos/'
+    path_labels_csv = '/home/ubuntu/Data/labels_framewise_csv.csv'
+    path_labels_list = '/home/ubuntu/Data/labels_framewise_list.pkl'    
+    path_frames = '/home/ubuntu/Data/Frames/'
+    checkpoint_path = "/home/ubuntu/checkpoints/training_deploy_softmax/cp.ckpt"
+
+
+# %%
+#Perform train-test-validation split(66-22-16)
+
+x = np.arange(1, 105)
+np.random.seed(42)
+np.random.shuffle(x)
+videos_validation = x[:16]
+videos_test = x[16: 16+22]
+videos_train = x[16+22: ]
+
+print(videos_train, len(videos_train))
+print(videos_test, len(videos_test))
+print(videos_validation, len(videos_validation))
