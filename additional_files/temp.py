225ed0673c1a0ac2068f64955f3a5e50b46e7aee
Siddhi <brahmbhattsiddhi@gmail.com>
2021-06-11 22:28:47 +0530

    sjcam-wifi

--- additional_files/temp.py
+++ additional_files/temp.py
@@ -1,8 +1,16 @@
-a = {1:10, 2:5, 3:6}
-
-for keys in a:
-    print(a[keys])
-
-print(sum(a.values()))
-
-print(list(a.keys()))
+import cv2
+cap = cv2.VideoCapture("rtsp://192.168.1.254/sjcam.mov")
+i = 0
+while(cap.isOpened()):
+    i = i+1
+    print(i)
+    ret, frame = cap.read()
+    cv2.imshow('frame', frame)
+    cv2.waitKey(20)
+    if i%200 == 0:
+        cap.release()
+        cap = cv2.VideoCapture("rtsp://192.168.1.254/sjcam.mov")
+    if i==2000:
+        break
+cap.release()
+cv2.destroyAllWindows()
